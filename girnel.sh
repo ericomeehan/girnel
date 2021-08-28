f89edbf612d0c2541259755d732fc82ec51272b7
Eric O Meehan <eric@eom.dev>
2021-08-28 17:22:31 +0000

    Added querying.

--- girnel.sh
+++ girnel.sh
@@ -59,6 +59,109 @@ setup()
     girnel_check
 }
 
-setup
+# Compose an Entry
+
+compose()
+{
+    touch /tmp/girnel.txt
+    vim /tmp/girnel.txt
+    ENTRY="$(cat /tmp/girnel.txt)"
+    if [[ $ENTRY == "" ]]
+    then
+        echo "No body written, canceling entry."
+        exit 0
+    fi
+}
+
+
+write()
+{
+    ENTRY="$(cat /tmp/girnel)"
+    rm /tmp/girnel
+    echo "========" >> $GIRNEL
+    echo "Author: ${PROJECT['author']}" >> $GIRNEL
+    echo "Date: ${PROJECT['date']}" >> $GIRNEL
+    echo "Time: ${PROJECT['time']}" >> $GIRNEL
+    echo "Branch: ${PROJECT['branch']}" >> $GIRNEL
+    echo "Commit: ${PROJECT['commit']}" >> $GIRNEL
+    echo "" >> $GIRNEL
+    echo "$ENTRY" >> $GIRNEL
+    echo "" >> $GIRNEL
+    echo "" >> $GIRNEL
+}
+
+# Query Previous Entries
+
+DB_SCHEMA="CREATE TABLE entries (
+        entry_id INTEGER PRIMARY KEY NOT NULL,
+        author TEXT,
+        date TEXT,
+        time TEXT,
+        branch TEXT,
+        hash TEXT,
+        message TEXT NOT NULL
+    );"
+
+insert()
+{
+    sqlite /tmp/girnel.db "
+        INSERT INTO entries (
+            author, date, time, branch, hash, mesage
+        ) VALUES (
+            \"${ENTRY['author']}\",
+            \"${ENTRY['date']}\",
+            \"${ENTRY['time']}\",
+            \"${ENTRY['branch']}\",
+            \"${ENTRY['commit']}\",
+            \"${ENTRY['message']}\"
+        );"
+}
+
+create_db()
+{
+    touch /tmp/girnel.db
+    sqlite /tmp/girnel.db "$DB_SCHEMA"
+
+    while IFS='' read -r line || [ -n "${line}" ]
+    do
+        if [[ $line =~ "========" ]]
+        then
+            if [[ ${ENTRY['message']} != "" ]]
+            then
+                insert
+            fi
+        elif [[ $line =~ "Author:" ]]
+        then
+            ENTRY['author']=$(echo "$line" | cut -d ' ' -f2-)
+        elif [[ $line =~ "Date:" ]]
+        then
+            ENTRY['date']=$(echo "$line" | cut -d ' ' -f2-)
+        elif [[ $line =~ "Time:" ]]
+        then
+            ENTRY['time']=$(echo "$line" | cut -d ' ' -f2-)
+        elif [[ $line =~ "Branch:" ]]
+
+        then
+            ENTRY['branch']=$(echo "$line" | cut -d ' ' -f2-)
+        elif [[ $line =~ "Commit:" ]]
+        then
+            ENTRY['commit']=$(echo "$line" | cut -d ' ' -f2-)
+        elif [[ $line != "" ]]
+        then
+            if [[ ${ENTRY['message']} == "" ]]
+            then
+                ENTRY['message']="$line"
+            else
+                ENTRY['message']="${ENTRY['message']}\n$line"
+            fi
+        fi
+    done < $GIRNEL
+}
+
+
+
+
+
+
 
 
